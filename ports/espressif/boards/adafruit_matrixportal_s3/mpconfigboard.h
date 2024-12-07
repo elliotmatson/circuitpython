893529110ec8b58baab20eb872b4a50f82187c1e
Elliot Matson <1711604+elliotmatson@users.noreply.github.com>
2024-12-07 17:08:46 -0600

    Update mpconfigboard.h

--- ports/espressif/boards/adafruit_matrixportal_s3/mpconfigboard.h
+++ ports/espressif/boards/adafruit_matrixportal_s3/mpconfigboard.h
@@ -11,7 +11,7 @@
 #define MICROPY_HW_BOARD_NAME       "Adafruit MatrixPortal S3"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
-#define MICROPY_HW_NEOPIXEL (&pin_GPIO4)
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO10)
 
 #define MICROPY_HW_LED_STATUS (&pin_GPIO13)
 
@@ -21,4 +21,4 @@
 #define DEFAULT_UART_BUS_RX (&pin_GPIO8)
 #define DEFAULT_UART_BUS_TX (&pin_GPIO18)
 
-#define DOUBLE_TAP_PIN (&pin_GPIO1)
+#define DOUBLE_TAP_PIN (&pin_GPIO0)
